bf833b99fba7e1b2660760fe44f0d52f125ea2af
zenjduke <zennajduke@gmail.com>
2018-07-11 11:01:05 -0500

    adding JAWS

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,24 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+const connection;
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "restaurant_db"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error("Error Connecting: " + err.stack);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log("Connected As ID " + connection.threadId);
 });
 
 module.exports = connection;
